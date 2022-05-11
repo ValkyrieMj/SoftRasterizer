e382402456b741bfd64cab017b9618b277cd7ea1
ValkyrieMj <1244249793@qq.com>
2022-05-11 22:27:29 +0800

    增加贴图

--- SoftRasterizer.cpp
+++ SoftRasterizer.cpp
@@ -14,6 +14,7 @@ const TGAColor green = TGAColor(0, 255, 0, 255);
 const int width = 800;
 const int height = 800;
 const int depth = 255;
+auto model = make_shared<Model>("obj1/african_head.obj");
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
     bool steep = false;
@@ -89,7 +90,7 @@ Matrix v2m(Vec3f v)
     return m;
 }
 
-//将x,y坐标放width×height，z坐标转成[0,255)
+//将x,y坐标放width×height，z坐标转成[0,255)，这里相机位置，直接这样就行了
 Matrix viewport(int x, int y, int w, int h)
 {
     Matrix m = Matrix::identity(4);
@@ -104,41 +105,87 @@ Matrix viewport(int x, int y, int w, int h)
     return m;
 }
 
-void triangle(const Vec3f pts[], vector<vector<float>> &zbuffer, TGAImage& image, TGAColor color)
-{
-    Vec2f bboxmin(numeric_limits<float>::max(), numeric_limits<float>::max());
-    Vec2f bboxmax(-numeric_limits<float>::max(), -numeric_limits<float>::max());
-    Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
-    //cout << pts[0].x<<" "<< pts[1].x << endl;
-    for (int i = 0; i < 3; i++) {
-        //for (int j = 0; j < 2; j++) {
-        //    bboxmin[j] = max(0.f, min(bboxmin[j], pts[i][j]));
-        //    bboxmax[j] = min(clamp[j], max(bboxmax[j], pts[i][j]));
-        //}
-        bboxmin[0] = max(0.f, min(bboxmin[0], pts[i].x));
-        bboxmin[1] = max(0.f, min(bboxmin[1], pts[i].y));
-        bboxmax[0] = min(clamp[0], max(bboxmax[0], pts[i].x));
-        bboxmax[1] = min(clamp[1], max(bboxmax[1], pts[i].y));
-    }
-    Vec3f P;
-    //cout << bboxmin[0] << bboxmin[1]<< bboxmax[0]<< bboxmax[1]<<endl;
-    //cout << bboxmin[0] <<" "<< bboxmax.x<<" "<< bboxmin.y<<" "<< bboxmax.y << endl;
-    for (P.x = bboxmin[0]; P.x < bboxmax[0]; P.x++) {
-        for (P.y = bboxmin[1]; P.y < bboxmax[1]; P.y++) {
-            //计算质心
-            if (P.x > 600 && P.y > 500)
+//void triangle(const Vec3f pts[], vector<vector<float>> &zbuffer, TGAImage& image, TGAColor color)
+//{
+//    Vec2f bboxmin(numeric_limits<float>::max(), numeric_limits<float>::max());
+//    Vec2f bboxmax(-numeric_limits<float>::max(), -numeric_limits<float>::max());
+//    Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
+//    //cout << pts[0].x<<" "<< pts[1].x << endl;
+//    for (int i = 0; i < 3; i++) {
+//        //for (int j = 0; j < 2; j++) {
+//        //    bboxmin[j] = max(0.f, min(bboxmin[j], pts[i][j]));
+//        //    bboxmax[j] = min(clamp[j], max(bboxmax[j], pts[i][j]));
+//        //}
+//        bboxmin[0] = max(0.f, min(bboxmin[0], pts[i].x));
+//        bboxmin[1] = max(0.f, min(bboxmin[1], pts[i].y));
+//        bboxmax[0] = min(clamp[0], max(bboxmax[0], pts[i].x));
+//        bboxmax[1] = min(clamp[1], max(bboxmax[1], pts[i].y));
+//    }
+//    Vec3f P;
+//    //cout << bboxmin[0] << bboxmin[1]<< bboxmax[0]<< bboxmax[1]<<endl;
+//    //cout << bboxmin[0] <<" "<< bboxmax.x<<" "<< bboxmin.y<<" "<< bboxmax.y << endl;
+//    for (P.x = bboxmin[0]; P.x < bboxmax[0]; P.x++) {
+//        for (P.y = bboxmin[1]; P.y < bboxmax[1]; P.y++) {
+//            //计算质心
+//            if (P.x > 600 && P.y > 500)
+//            {
+//                P.x += 0.01;
+//            }
+//            Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+//            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+//            P.z = 0;
+//            for (int i = 0; i < 3; i++) P.z += pts[i].z * bc_screen[i];
+//           // cout << P.z<<" "<< zbuffer[int(P.x)][int(P.y)] << endl;
+//            //if (zbuffer[int(P.x)][int(P.y)] < P.z) {
+//                image.set(P.x, P.y, color);
+//                zbuffer[int(P.x)][int(P.y)] = P.z;
+//           // }
+//        }
+//    }
+//}
+
+void triangle(Vec3i t0, Vec3i t1, Vec3i t2, Vec2i uv0, Vec2i uv1, Vec2i uv2, TGAImage& image, float intensity, vector<vector<float>>& zbuffer) {
+    if (t0.y == t1.y && t0.y == t2.y) return;
+    //分割成两个三角形
+    if (t0.y > t1.y) { std::swap(t0, t1); std::swap(uv0, uv1); }
+    if (t0.y > t2.y) { std::swap(t0, t2); std::swap(uv0, uv2); }
+    if (t1.y > t2.y) { std::swap(t1, t2); std::swap(uv1, uv2); }
+    //用高度做循环控制
+    int total_height = t2.y - t0.y;
+    for (int i = 0; i < total_height; i++) {
+        //判断属于哪一部分以确定高度
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        //计算当前的比例
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
+        //A表示t0与t2之间的点
+        //B表示t0与t1之间的点
+        Vec3i A = t0 + Vec3f(t2 - t0) * alpha;
+        Vec3i B = second_half ? t1 + Vec3f(t2 - t1) * beta : t0 + Vec3f(t1 - t0) * beta;
+        //计算UV
+        Vec2i uvA = uv0 + (uv2 - uv0) * alpha;
+        Vec2i uvB = second_half ? uv1 + (uv2 - uv1) * beta : uv0 + (uv1 - uv0) * beta;
+        //保证B在A的右边
+        if (A.x > B.x) { std::swap(A, B); }// std::swap(uvA, uvB);}
+        //用横坐标作为循环控制，对这一行进行着色
+        for (int j = A.x; j <= B.x; j++) {
+            //计算当前点在AB之间的比例
+            float phi = B.x == A.x ? 1. : (float)(j - A.x) / (float)(B.x - A.x);
+            //计算出当前点的坐标,A，B保存了z轴信息
+            Vec3i   P = Vec3f(A) + Vec3f(B - A) * phi;
+            Vec2i uvP = uvA + (uvB - uvA) * phi;
+            if (P.x < width && P.y < height)
             {
-                P.x += 0.01;
+                //计算当前zbuffer下标=P.x+P.y*width
+                //int idx = P.x + P.y * width;
+                //当前点的z大于zbuffer信息，覆盖掉，并更新zbuffer
+                if (zbuffer[P.x][P.y] < P.z) {
+                    zbuffer[P.x][P.y] = P.z;
+                    TGAColor color = model->diffuse(uvP);
+                    image.set(P.x, P.y, TGAColor(color.r * intensity, color.g * intensity, color.b * intensity));
+                }
             }
-            Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
-            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-            P.z = 0;
-            for (int i = 0; i < 3; i++) P.z += pts[i].z * bc_screen[i];
-           // cout << P.z<<" "<< zbuffer[int(P.x)][int(P.y)] << endl;
-            //if (zbuffer[int(P.x)][int(P.y)] < P.z) {
-                image.set(P.x, P.y, color);
-                zbuffer[int(P.x)][int(P.y)] = P.z;
-           // }
         }
     }
 }
@@ -149,26 +196,52 @@ Vec3f world2screen(Vec3f v) {
 
 int main()
 {
-    auto model = make_shared<Model>("obj1/african_head.obj");
+
     TGAImage image(width, height, TGAImage::RGB);
+    Vec3f camara(0, 0, 3);
     Vec3f light_dir(0, 0, -1);
     vector<vector<float>> zbuffer(width,vector<float>(height, -std::numeric_limits<float>::max()));
+    //for (int i = 0; i < model->nfaces(); i++) {
+    //    vector<int> face = model->face(i);
+    //    Vec3f screen_coords[3];
+    //    Vec3f world_coords[3];
+    //    for (int j = 0; j < 3; j++) {
+    //        Vec3f v = model->vert(face[j]);
+    //        screen_coords[j] = world2screen(model->vert(face[j]));
+    //        world_coords[j] = v;
+    //        //screen_coords[j] = Vec3f(int((v.x + 1.) * width / 2+.5), int((v.y + 1.) * height / 2+.5), v.z);
+    //    }
+    //    Vec3f n = (world_coords[2] - world_coords[0])^(world_coords[1] - world_coords[0]);
+    //    n.normalize();
+    //    float intensity = n * light_dir;
+    //    //intensity = 1;
+    //    if(intensity > 0)
+    //        triangle(screen_coords,zbuffer, image, TGAColor(255 * intensity, 255 * intensity, 255 * intensity, 255));
+    //}
+    Matrix Projection = Matrix::identity(4);
+    Projection[3][2] = -1.f / camara.z;
+    Matrix ViewPort = viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
     for (int i = 0; i < model->nfaces(); i++) {
         vector<int> face = model->face(i);
-        Vec3f screen_coords[3];
+        Vec3i screen_coords[3];
         Vec3f world_coords[3];
         for (int j = 0; j < 3; j++) {
             Vec3f v = model->vert(face[j]);
-            screen_coords[j] = world2screen(model->vert(face[j]));
+            screen_coords[j] = m2v(ViewPort * Projection * v2m(v));
             world_coords[j] = v;
-            //screen_coords[j] = Vec3f(int((v.x + 1.) * width / 2+.5), int((v.y + 1.) * height / 2+.5), v.z);
         }
         Vec3f n = (world_coords[2] - world_coords[0])^(world_coords[1] - world_coords[0]);
         n.normalize();
         float intensity = n * light_dir;
-        intensity = 1;
-        //if(intensity > 0)
-            triangle(screen_coords,zbuffer, image, TGAColor(255 * intensity, 255 * intensity, 255 * intensity, 255));
+        intensity = std::min(std::abs(intensity), 1.f);
+        if (intensity > 0) {
+            Vec2i uv[3];
+            for (int k = 0; k < 3; k++) {
+                uv[k] = model->uv(i, k);
+            }
+            //绘制三角形
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], uv[0], uv[1], uv[2], image, intensity, zbuffer);
+        }
     }
     image.flip_vertically();
     image.write_tga_file("output.tga");
